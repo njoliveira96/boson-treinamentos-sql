f6a41981bc319c7c8556ef397f3fb1e7698fcdc1
Nadson <new.nadson@gmail.com>
2024-12-12 17:59:40 -0300

    update1-insert-sql

--- insert.sql
+++ insert.sql
@@ -23,20 +23,30 @@ INSERT INTO Autor (NomeAutor, SobrenomeAutor)
 VALUES ('Umberto', 'Eco'), ('Daniel', 'Barret'), ('Gerald', 'Carter'), ('Mark', 'Sobell'), ('William', 'Stanek'), ('Christine', 'Bresnahan'), ('William', 'Gibson'), ('James', 'Joyce'), 
 ('Jhon', 'Emsley'), ('José', 'Saramago'), ('Richard', 'Silverman'), ('Robert','Byrnes'), ('Jay', 'Ts'), ('Robert', 'Eckstein'), ('Paul', 'Horowitz'), ('Winfield','Hill'), ('Joel','Murach'),
 ('Paul', 'Scherz'), ('Simon', 'Monk'), ('George', 'Orwell'), ('Ítalo', 'Calvino'), ('Machado', 'de Assis'), ('Oliver', 'Sacks'), ('Ray', 'Bradbury'), ('Walter', 'Isaacson'), ('Benjamin', 'Graham'),
-('Júlio', 'Verne'), ('Marcelo','Gleiser'), ('Harri','Lorenzi'), ('Humphrey', 'Carpenter'), ('Isaac', 'Asimov'), ('Aldous', 'Huxley'), ()
+('Júlio', 'Verne'), ('Marcelo','Gleiser'), ('Harri','Lorenzi'), ('Humphrey', 'Carpenter'), ('Isaac', 'Asimov'), ('Aldous', 'Huxley'), ('Arthur', 'Conan Doyle'), ('Blaise', 'Pascal'), ('Jostein','Gaarder'),
+('Stephen', 'Hawking'), ('Stephen','Jay Gould'), ('Neil','De Grasse Tyson'), ('Charles','Darwin'), ('Alan','Turing'), ('Arthur','C. Clarke');
 
+SELECT * FROM Autor;
+/*
+INSERT INTO Livro (NomeLivro, ISBN13, DtaPub, PrecoLivro, NumeroPaginas, IdAssunto, IdEditora)
+VALUES('A arte da eletrônica', '9788582604342','20170308', 300.74, 1160, 3, 24);
+*/
+INSERT INTO Livro (NomeLivro, ISBN13, DtaPub, PrecoLivro, NumeroPaginas, IdAssunto, IdEditora)
+VALUES('Vinte Mil Léguas Submarinas', '9788582850022','2014-09-16', 24.50, 448, 1, 16), -- Júlio Verne
+('O investidor inteligente','9788595080805', '2016-01-25', 79.90, 450, 7, 6); -- Benjamin Graham
 
+SELECT * FROM Livro;
 
+/*DELETE FROM Livro
+WHERE ISBN13 = 9788595080805;*/ -- TIVE QUE REINSERIR O A LINHA 3, AJUSTEI O TÍTULO DO LIVRO
 
-
-
-
-
-
-
-
-
-
+SELECT NomeLivro, ISBN13, DtaPub, PrecoLivro, NumeroPaginas, IdEditora, IdAssunto
+FROM OPENROWSET(
+	BULK 'C:\Users\nadson.oliveira\Documents\estudos\boson-treinamentos-sql\Livros.csv',
+	FORMATFILE = 'C:\Users\nadson.oliveira\Documents\estudos\boson-treinamentos-sql\Formato.xml',
+	CODEPAGE = '65001', -- UTF-8
+	FIRSTROW = 2
+) AS LivrosCSV;
 
 
 -- DELETA LINHAS DA TABELA
