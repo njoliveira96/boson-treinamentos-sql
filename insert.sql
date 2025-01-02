bf29a06cdb6d86d95277935df49b909fd54d6924
Nadson <new.nadson@gmail.com>
2025-01-02 16:39:03 -0300

    update5-insert-sql

--- insert.sql
+++ insert.sql
@@ -5,14 +5,13 @@ VALUES
 ('Ficção Científica'), ('Botânica'),('Eletrônica'),('Matemática'), ('Aventura'), ('Romance'), ('Finanças'), 
 ('Gastronomia'), ('Terror'), ('Administração'), ('Informática'), ('Suspense');
 
--- VERIFICAÇÃO
 SELECT * FROM Assunto;
-SELECT * FROM Editora;
-SELECT * FROM Autor;
 
 INSERT INTO Editora (NomeEditora)
 VALUES
 ('Prentice Hall'), ('O´Reilly');
+
+SELECT * FROM Editora;
 
 INSERT INTO Editora (NomeEditora)
 VALUES
@@ -28,20 +27,12 @@ VALUES ('Umberto', 'Eco'), ('Daniel', 'Barret'), ('Gerald', 'Carter'), ('Mark', 
 
 SELECT * FROM Autor;
 
-INSERT INTO Livro (NomeLivro, ISBN13, DtaPub, PrecoLivro, NumeroPaginas, IdAssunto, IdEditora)
-VALUES('A arte da eletrônica', '9788582604342','20170308', 300.74, 1160, 3, 24);
-
-INSERT INTO Livro (NomeLivro, ISBN13, DtaPub, PrecoLivro, NumeroPaginas, IdAssunto, IdEditora)
-VALUES('Vinte Mil Léguas Submarinas', '9788582850022','2014-09-16', 24.50, 448, 1, 16), -- Júlio Verne
+INSERT INTO Livro (NomeLivro, ISBN13, DataPub, PrecoLivro, NumeroPaginas, IdEditora, IdAssunto)
+VALUES('A arte da eletrônica', '9788582604342','20170308', 300.74, 1160, 24, 3), ('Vinte Mil Léguas Submarinas', '9788582850022','2014-09-16', 24.50, 448, 16, 1), -- Júlio Verne
 ('O investidor inteligente','9788595080805', '2016-01-25', 79.90, 450, 7, 6); -- Benjamin Graham
 
-SELECT * FROM Livro;
-
-/*DELETE FROM Livro
-WHERE ISBN13 = 9788595080805;*/ -- TIVE QUE REINSERIR A LINHA 3, AJUSTEI O TÍTULO DO LIVRO
-
-INSERT INTO Livro (NomeLivro, ISBN13, DtaPub, PrecoLivro, NumeroPaginas, IdEditora, IdAssunto)
-SELECT NomeLivro, ISBN13, DtaPub, PrecoLivro, NumeroPaginas, IdEditora, IdAssunto
+INSERT INTO Livro (NomeLivro, ISBN13, DataPub, PrecoLivro, NumeroPaginas, IdEditora, IdAssunto)
+SELECT NomeLivro, ISBN13, DataPub, PrecoLivro, NumeroPaginas, IdEditora, IdAssunto
 FROM OPENROWSET(
 	BULK 'C:\boson-treinamentos-sql\Livros.csv',
 	FORMATFILE = 'C:\boson-treinamentos-sql\Formato.xml',
@@ -49,9 +40,21 @@ FROM OPENROWSET(
 	FIRSTROW = 2
 ) AS LivrosCSV;
 
+SELECT * FROM Livro;
+
 INSERT INTO LivroAutor (IdLivro, IdAutor)
-VALUES
-(100,15),(
+VALUES (100,15),(100,16),(101,27),(102,26),(103,41),(104,24),(105,32),(106,20),(107,27),(108,1),(109,22),(110,10),(111,21),(112,5),(113,10),(114,8),(115,18),(115,19),(116,31),(117,22);
+
+SELECT * FROM LivroAutor;
+
+SELECT * FROM Livro;
+
+DELETE FROM Livro;
+
+SELECT * FROM Autor;
+
+DELETE FROM LivroAutor;
+
 
 
 
